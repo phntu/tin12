1918ab3f1f8c5564e1604acf814e7de729a6ca15
phntu <phanlecamtu12@gmail.com>
2025-03-28 10:11:31 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let p = pw.value
+  if (u=="phuonglinh" && p=="2807")
+  {
+    window.open("home.html")
+    document.getElementByTd(compiler).style.display="block"
+  }
+}
